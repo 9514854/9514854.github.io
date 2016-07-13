ee3f9466bf9ba15dbcd408335f0dc1b5ea2d6c5f
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-13 14:27:35 +0000

    changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var name = document.getElementById("name").value;
 var btn = document.getElementById("btn");
 btn.onclick = function() {
+    var name = document.getElementById("name").value;
     alert(name);
 };
