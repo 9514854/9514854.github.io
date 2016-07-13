685ecae272ff7c7c6d7c5da9f312187be5159a3e
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-13 17:18:24 +0000

    c

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var btn = document.getElementById("btn");
 btn.onclick = function() {
-    var name = document.getElementById("name").value.toUpperCase;
+    var name = document.getElementById("name").value.ToUpper;
     if(name == "CONNER CARLILE") {
         alert(name + " sucks");
     }
