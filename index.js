a3ef38d598f968c2de0726cc244afc1300592871
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-14 22:56:26 +0000

    c

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ btn.onclick = function() {
     for(var i = 0; i < name.length; i++) {
         nick.push(name[i]);
     }
-    if(name == "conner carlile") {
-        alert(name + " sucks");
-    }
-    else {
+        var m = nick.splice(0, 1);
         var n = nick.splice(-2, 1);
         var a = nick.splice(-1, 1);
         document.getElementById("tag").innerHTML = '';
-        document.getElementById("tag").innerHTML += "Your nickname is " + nick[0] + n + a;
-    }
+        document.getElementById("tag").innerHTML += "Your nickname is " + m + n + a;
     document.getElementById("name").value = '';
     nick = [];
 };
