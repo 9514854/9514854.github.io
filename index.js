01b16f6c7490a181b0bc40f10ebfcde645ce01cf
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-14 23:17:09 +0000

    c

--- index.js
+++ index.js
@@ -11,11 +11,10 @@ btn.onclick = function() {
     }
     else {
         var m = nick.splice(0, 1);
-        var mn = m.toUpperCase();
         var n = nick.splice(-2, 1);
         var a = nick.splice(-1, 1);
         document.getElementById("tag").innerHTML = '';
-        document.getElementById("tag").innerHTML += "Your nickname is " + mn + n + a;
+        document.getElementById("tag").innerHTML += "Your nickname is " + m + n + a;
     }
     document.getElementById("name").value = '';
     nick = [];
