b75c0d6da82bfabfb95cb16b567afe5f12421e37
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-14 23:29:10 +0000

    c

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ document.getElementById("btn")
     .addEventListener("keyup", function(event) {
     event.preventDefault();
     if (event.keyCode == 13) {
-        document.getElementById("btn").click();
-    }
-});
 btn.onclick = function() {
     var name = document.getElementById("name").value.toLowerCase();
     for(var i = 0; i < name.length; i++) {
@@ -26,3 +23,26 @@ btn.onclick = function() {
     document.getElementById("name").value = '';
     nick = [];
 };
+}
+else {
+    btn.onclick = function() {
+    var name = document.getElementById("name").value.toLowerCase();
+    for(var i = 0; i < name.length; i++) {
+        nick.push(name[i]);
+    }
+    if(name == "conner") {
+        document.getElementById("tag").innerHTML = '';
+        document.getElementById("tag").innerHTML += "Your nickname is Awesome";
+    }
+    else {
+        var m = nick.splice(0, 1);
+        var n = nick.splice(-2, 1);
+        var a = nick.splice(-1, 1);
+        document.getElementById("tag").innerHTML = '';
+        document.getElementById("tag").innerHTML += "Your nickname is " + m + n + a;
+    }
+    document.getElementById("name").value = '';
+    nick = [];
+};
+};
+}
