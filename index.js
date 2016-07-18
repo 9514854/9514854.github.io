3131392c2dd7c0f26647b532ef9c879bdbaa482e
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-18 17:14:28 +0000

    updating

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var btn = document.getElementById("btn");
 var conner = ["Awesome", "Conman", "Con", "Concon"];
+var brandon = ["Brando", "Bran", "Branny", "Brandinie"];
 var randomNicknameGenerators = [
     function(name) { return name[0] + name[1] + name[name.length - 1] },
     function(name) { return name[0] + name[Math.floor(name.length / 2)] + name[name.length - 1] }
-]
+];
 function ranElement(ran) {
     return ran[Math.floor(Math.random()*ran.length)];
 }
@@ -23,6 +24,7 @@ btn.onclick = function() {
     var name = document.getElementById("name").value.toLowerCase();
     
     if (name == "conner") newNick(ranElement(conner));
+    else if (name == "brandon") newNick(ranElement(brandon));
     else if (name == "lance") newNick("Sick");
     else newNick(ranElement(randomNicknameGenerators)(name));
     
