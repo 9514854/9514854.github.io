a278fccb5d1818af14a2a3c855a6706666d0d6af
Samuel Kaleb Crowford <virkenation@gmail.com>
2016-07-18 16:29:17 +0000

    c

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var btn = document.getElementById("btn");
+
+function newNick(nick) {
+    document.getElementById("tag").innerHTML = "Your nickname is " + nick;
+}
+
 document.getElementById("name")
     .addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        document.getElementById("btn").click();
-    }
-});
+        event.preventDefault();
+        if (event.keyCode == 13) {
+            document.getElementById("btn").click();
+        }
+    });
 btn.onclick = function() {
     var name = document.getElementById("name").value.toLowerCase();
-    if(name == "conner") {
-        document.getElementById("tag").innerHTML = '';
-        document.getElementById("tag").innerHTML += "Your nickname is Awesome";
-    }
-    else if (name == "lance") {
-       document.getElementById("tag").innerHTML = '';
-        document.getElementById("tag").innerHTML += "Your nickname is Stoned"; 
-    }
-    else {
-        document.getElementById("tag").innerHTML = '';
-        document.getElementById("tag").innerHTML += "Your nickname is " + name[0] + name[1] + name[name.length-1];
-    }
+    
+    if (name == "conner") newNick("Awesome");
+    else if (name == "lance") newNick("Sick");
+    else newNick(name[0] + name[1] + name[name.length - 1]);
+    
     document.getElementById("name").value = '';
 };
